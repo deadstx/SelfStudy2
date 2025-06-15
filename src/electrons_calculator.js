838f13b03563f367430e12c7feac1b120858c173
elenaDord <if24b156@technikum-wien.at>
2025-06-15 14:45:45 +0200

    Production code, Dordevic Elena

--- src/electrons_calculator.js
+++ src/electrons_calculator.js
@@ -2,9 +2,21 @@
 
 class ElectronsCalculator {
     calculate(dice) {
-        // TODO: Implement the electrons calculation
-        return 0;
+        let total = 0;
+
+        for (const die of dice) {
+            if (die === 3) {
+                total += 2;
+            } else if (die === 5) {
+                total += 4;
+            } else if (die === 2 || die === 1 || die === 4 || die === 6 || die === 7 || die === 0) {
+                total += 0;
+            }
+        }
+
+        return total;
     }
 }
+
 
 module.exports = ElectronsCalculator;
