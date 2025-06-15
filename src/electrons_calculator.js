641ed523d99dc2ca180ebd8aecf2e8587fa658ce
lars <if24b115@technikum-wien.at>
2025-06-15 15:56:33 +0200

    Production code, Hosnedl Lars

--- src/electrons_calculator.js
+++ src/electrons_calculator.js
@@ -2,6 +2,15 @@
 
 class ElectronsCalculator {
     calculate(dice) {
+
+        if (!Array.isArray(dice)) {
+            return "Input must be an array of numbers";
+        }
+
+        if (dice.length < 4 || dice.length > 6) {
+            return "Input must be an array of 4-6 numbers";
+        }
+
         let total = 0;
 
         for (const die of dice) {
@@ -14,6 +23,5 @@ class ElectronsCalculator {
         return total;
     }
 }
-
 
 module.exports = ElectronsCalculator;
